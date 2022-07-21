86f273ac75cb0c9ad1175db5820fc76832f05b6e
lotlouischoitslab <louis.sw.cho@gmail.com>
2022-07-21 15:33:50 -0500

    add numerical methods

---
+++ 00Artificial_Intelligence/Numerical_Methods/07_Random_Numbers_Monte_Carlo_Methods/7_2_calculating_a_volume_of_intersection.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan  5 00:15:14 2022
+
+@author: LOTLOUISCHO
+"""
+
+import numpy as np
+
+# # Part 1
+
+def insideCylinders(pos,r):
+    x, y, z = pos
+    # Return true if the point is inside the Steinmetz solid
+    # write some code here
+    return 1
+
+# # Part 2
+
+def prob_inside_volume(N,r):
+    # Calculate the probability that any point is inside the Steinmetz solid
+    return 1
+
+## Part 3
+N = 1000
+r = 1
+# Determine approximated volume
+
